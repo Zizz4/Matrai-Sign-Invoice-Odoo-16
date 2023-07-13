da6cb0c5bcfd327b90385e524eaadbec4f6d5b4e
kosmoswave <rizki@kosmoswave.com>
2023-07-13 10:46:05 +0700

    matrai sign for invoice odoo 16

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Matrai Sign Invoice',
+    'version': '16.0',
+    'summary': 'Sign for Invoice report',
+    'description': 'Description',
+    'category': 'Productivity',
+    'author': 'Muhamad Syahril Aziz',
+    'license': 'LGPL-3',
+    'depends': ['account'],
+    'data': ['report/invoice_sign_kosmos.xml'],
+    'installable': True,
+    'auto_install': False
+}
